f36ac2d9bfefc8442e3615b9b6f401e96de84113
parkjise <monories79@gmail.com>
2024-02-29 14:36:08 +0900

    button

--- course_code/src/App.jsx
+++ course_code/src/App.jsx
@@ -1,16 +1,12 @@
-import { AvatarFunctionality, AvatarCustomization, BadgeFunctionality, BadgeCustomization, ChipFunctionality, ChipCustomization, DividerFunctionality, DividerCustomization, ListFunctionality, ListCustomization, MuiIconsFunctionality, MuiIconsCustomization, SvgIconFunctionality, SvgIconCustomization, TableFunctionality, TableCustomization, TooltipFunctionality, TooltipCustomization, TypographyFunctionality, TypographyCustomization } from "./components/data_display"
-import { AlertFunctionality, AlertCustomization, BackdropFunctionality, BackdropCustomization, DialogFunctionality, DialogCustomization, ProgressFunctionality, ProgressCustomization, SkeletonFunctionality, SkeletonCustomization, SnackbarFunctionality, SnackbarCustomization } from "./components/feedback"
-import { AutocompleteFunctionality, AutocompleteCustomization, ButtonFunctionality, ButtonCustomization, ButtonGroupFunctionality, ButtonGroupCustomization, CheckboxFunctionality, CheckboxCustomization, FabFunctionality, FabCustomization, FormComponentsFunctionality, FormComponentsCustomization, RadioFunctionality, RadioCustomization, RatingFunctionality, RatingCustomization, SelectFunctionality, SelectCustomization, SliderFunctionality, SliderCustomization, SwitchFunctionality, SwitchCustomization, TextfieldFunctionality, TextfieldCustomization, ToggleButtonFunctionality, ToggleButtonCustomization } from "./components/inputs"
-import { BoxFunctionality, BoxCustomization, ContainerFunctionality, ContainerCustomization, GridFunctionality, GridDemo, GridCustomization, ImageListFunctionality, ImageListCustomization, StackFunctionality, StackCustomization } from "./components/layout"
-import { BottomNavigationFunctionality, BottomNavigationCustomization, BreadcrumbsFunctionality, BreadcrumbsCustomization, DrawerFunctionality, DrawerCustomization, LinkFunctionality, LinkCustomization, MenuFunctionality, MenuCustomization, PaginationFunctionality, PaginationCustomization, SpeedDialFunctionality, SpeedDialCustomization, StepperFunctionality, StepperCustomization, TabsFunctionality, TabsCustomization } from "./components/navigation"
-import { AccordionFunctionality, AccordionCustomization, AppBarFunctionality, AppBarCustomization, CardFunctionality, CardCustomization, PaperFunctionality, PaperCustomization } from "./components/surfaces"
-import { ClickAwayListenerFunctionality, CssBaselineFunctionality, CssBaselineCustomization, ModalFunctionality, ModalCustomization, NoSsrFunctionality, PopperFunctionality, PortalFunctionality, TextareaAutosizeFunctionality, TransitionsFunctionality, useMediaQueryFunctionality, useScrollTriggerFunctionality } from "./components/utils"
+
+import {Button} from "@mui/material"
 
 export default function App() {
   return (
-    <>
-      <h1>Welcome to Mastering MUI: Component Customization</h1>
-      <SelectCustomization />
-    </>
+    <div className="App">
+    <h1>Hello CodeSandbox</h1>
+    <h2>Start editing to see some magic happen!</h2>
+    <Button color="primary" variant="contained">My Button</Button>
+  </div>
   )
 }
