c562b483e705015a6083011010b1ac6efaeb57ac
parkjise <monories79@gmail.com>
2024-03-08 13:53:09 +0900

    sx property

--- course_code/src/App.jsx
+++ course_code/src/App.jsx
@@ -2,11 +2,28 @@
 import {Button} from "@mui/material"
 
 export default function App() {
+  // margin-bottom
+  // marginBottom
   return (
     <div className="App">
     <h1>Hello CodeSandbox</h1>
-    <h2>Start editing to see some magic happen!</h2>
-    <Button color="primary" variant="contained">My Button</Button>
+    <h2 style={{color:"red",marginBottom:"140px"}}>Start editing to see some magic happen!</h2>
+    <Button 
+    color="primary" 
+    variant="contained" 
+    sx={{
+      color:"warning.contrastText",
+      borderColor:(theme) => theme.palette.primary.main,
+      borderStyle:"solid",
+      borderWidth:"5px",
+      bgcolor:"green",
+      px:"150px",
+      typography:"h1",
+      fontSize:"18px",
+      margin:50,
+      fontWeight:""
+      }}>
+    My Button</Button>
   </div>
   )
 }
