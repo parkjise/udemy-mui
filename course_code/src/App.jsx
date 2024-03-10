207bf638ec214015621ebad8e293a9be94c8847b
parkjise <monories79@gmail.com>
2024-03-10 16:46:54 +0900

    sx

--- course_code/src/App.jsx
+++ course_code/src/App.jsx
@@ -1,9 +1,30 @@
 
+import { useState } from "react"
 import {Button} from "@mui/material"
 
 export default function App() {
+
+  const [open,setOpen] = useState(false);
+
   // margin-bottom
-  // marginBottom
+  // style tag : marginBottom
+  //  sx prop : mb
+
+  // fontSize : 10 -> fontSize : "10px"
+  // fontSize : h5.fontSize -> theme.typography.h5.fontSize
+
+  // color : primary.main -> theme.palette.primary.main
+
+
+  // width/height : [0,1] -> %
+  // width/height : 50 -> 50px
+  // width/height : 25rem -> 25rem
+
+
+  // maxWidth : md -> theme.breakpoints.values[md]
+  // borderRadius: 4 -> theme.shape.borderRadius * 4(val) 
+  // boxShadow/dropShadow:20 -> theme.shadows[20]
+  // zIndex:"modal" -> theme.zIndex["modal"]
   return (
     <div className="App">
     <h1>Hello CodeSandbox</h1>
@@ -21,9 +42,42 @@ export default function App() {
       typography:"h1",
       fontSize:"18px",
       margin:50,
-      fontWeight:""
+      fontWeight:"",
+      width:0.25,
+      maxWidth:"md",
+      borderRadius:2,
+      boxShadow:20,
+      // zIndex:"modal"
+      zIndex:(theme) => theme.zIndex["modal"] + 1,
       }}>
     My Button</Button>
+    <Button
+    onClick={() => setOpen(!open)} 
+    sx={[
+      {
+        borderColor:(theme) => theme.typography.h1.color,
+        borderStyle:"solid",
+        borderWidth:"2px",
+        // width:1,
+        maxWidth:"xl",
+        borderRadius:2,
+        boxShadow:20,
+        bgcolor:{xs:"green",sm:"purple"},
+        // bgcolor:open ? "red" : "black",
+        zIndex:(theme) => theme.zIndex["modal"] + 1,
+        "&:hover":{
+          bgcolor:"orange",
+        },
+        "& p,h1":{
+          color:"#000",
+        }
+      },
+      open && {
+        bgcolor:"red",
+        fontSize:"2rem",
+      },
+      ]}>
+    My Button2</Button>
   </div>
   )
 }
